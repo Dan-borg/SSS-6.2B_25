17c7fbcc14b50ec1c89ba0f95df320df28408d4c
Danayhia Borg <BORGDANAYHIA7.com>
2025-11-19 12:51:57 +0100

    Created some basic routes

--- contactApp/routes/web.php
+++ contactApp/routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    //the '/' is the home page
     return view('welcome');
+});
+
+Route::get('/contacts', function () {
+    // http://172.20.10.2:8000/contacts . will display all contacts only
+    return "<h1> All Contacts </h1>";
+});
+
+Route::get('/contacts/create', function () {
+    return "<h1> Create New Contact </h1>";
+});
+
+Route::get('/contacts/{id}', function ($id) {
+    return App\Models\Contact::find($id);
 });
